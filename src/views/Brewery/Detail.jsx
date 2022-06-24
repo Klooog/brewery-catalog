0ddb467f420acae0372dc877b64ee9aaf7ec1814
Kegan Hunter <khunter@khunter-mac.local>
2022-06-24 16:32:50 -0400

    add searchable list view and detail view

--- src/views/Brewery/Detail.jsx
+++ src/views/Brewery/Detail.jsx
@@ -1,18 +1,36 @@
-import { Link, useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 
 export default function BreweryDetail() {
   const { id } = useParams();
+  // State
+  const [breweryDetails, setBreweryDetails] = useState([]);
 
+  // GET Brewery Details https://api.openbrewerydb.org/breweries/{$id}
+  const fetchBreweryDetails = async (url) => {
+    const res = await fetch(url);
+
+    const brewery = await res.json();
+
+    setBreweryDetails(brewery);
+  };
+  useEffect(() => {
+    fetchBreweryDetails(`https://api.openbrewerydb.org/breweries/${id}`);
+  }, []);
   return (
-    <main>
-      <h1>Brewery {id}</h1>
-      <p>Brewtown, Oregon 12345</p>
-      <p>United States</p>
-      <p>8005551234</p>
-      <p>
-        <a href='https://example.com'>View Website</a>
-      </p>
-      <Link to='/breweries'>Back to Breweries</Link>
-    </main>
+    breweryDetails && (
+      <main>
+        <h1>{breweryDetails.name}</h1>
+        <p>{`${breweryDetails.city}, ${breweryDetails.state} ${breweryDetails.postal_code}`}</p>
+        <p>{breweryDetails.country}</p>
+        <p>{breweryDetails.phone}</p>
+        {breweryDetails.website_url !== null && (
+          <p>
+            <a href={breweryDetails.website_url}>View Website</a>
+          </p>
+        )}
+        <Link to="/breweries">Back to Breweries</Link>
+      </main>
+    )
   );
 }
--- src/views/Brewery/List.jsx
+++ src/views/Brewery/List.jsx
@@ -1,18 +1,71 @@
-import { Link } from 'react-router-dom';
+import { useState, useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function BreweryList() {
+  // State
+  const [initialBreweries, setInitialBreweries] = useState([]);
+  const [loading, setLoading] = useState(false);
+  // Refs
+  const searchRef = useRef();
+
+  // call API with any endpoint and store
+  const fetchBreweryList = async (url) => {
+    setLoading(true);
+    const res = await fetch(url);
+
+    const breweryList = await res.json();
+    console.log(url);
+    setInitialBreweries(breweryList);
+
+    setLoading(false);
+  };
+
+  // Initial api call
+  useEffect(() => {
+    fetchBreweryList("https://api.openbrewerydb.org/breweries?per_page=10");
+  }, []);
+
+  // Search Query GET ten results
+  const handleSearch = (e) => {
+    e.preventDefault();
+
+    fetchBreweryList(
+      `https://api.openbrewerydb.org/breweries/search?query=${searchRef.current.value}&per_page=10`
+    );
+  };
+
+  // Clear search input and reload initial data
+  const clearSearch = (e) => {
+    e.preventDefault();
+    searchRef.current.value = "";
+
+    // Return Recent Data
+    fetchBreweryList("https://api.openbrewerydb.org/breweries?per_page=10");
+  };
   return (
     <main>
       <h1>Brewery Catalog</h1>
-      <form>
-        <input type='text' name='search' placeholder='Find a brewery' />
-        <button type='submit'>Search</button>
-        <button type='reset'>Reset</button>
+      <form onSubmit={(e) => handleSearch(e)}>
+        <input
+          type="text"
+          name="search"
+          placeholder="Find a brewery"
+          ref={searchRef}
+        />
+        <button type="submit">Search</button>
+        <button onClick={(e) => clearSearch(e)}>Reset</button>
       </form>
       <ul>
-        <li>
-          <Link to='/breweries/1'>Brewery 1</Link> - Brewtown, OR
-        </li>
+        {loading
+          ? "Loading..."
+          : initialBreweries.length === 0
+          ? "No Results Available"
+          : initialBreweries.map((brewery) => (
+              <li key={brewery.id}>
+                <Link to={`/breweries/${brewery.id}`}>{brewery.name}</Link>-{" "}
+                {brewery.city}, {brewery.state}
+              </li>
+            ))}
       </ul>
     </main>
   );
